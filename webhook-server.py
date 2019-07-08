a579ab140e0417bf8da7d37133c833eca57557b0
devendrajha <dev.jha_19@yahoo.com>
2019-07-08 14:01:11 +0530

    Add files via upload

---
+++ webhook-server.py
@@ -0,0 +1,79 @@
+#!flask/bin/python
+#!/usr/bin/env python3
+import six
+from flask import Flask, request
+from flask_json import json_response
+from model import Dao
+
+app = Flask(__name__, static_url_path="")
+
+
+
+
+app = Flask(__name__)
+app.config['JSON_ADD_STATUS'] = True
+app.config['JSON_STATUS_FIELD_NAME'] = 'http_status'
+
+
+@app.route('/snp/webhooks/<sku>', methods=['GET'])
+@app.route('/snp/webhooks', methods=['GET'])
+def get_documents(sku=None):
+    if sku is None:
+        dao = Dao()
+        result = dao.find_document()
+        elemcount = len(result)
+        #result.append({'count':elemcount})
+        if result is not None:
+            return json_response(documents=result,count=elemcount)
+        else:
+            return json_response(http_status=500, message='Some errors occurred in the process')
+    else:
+        dao = Dao()
+        try:
+            #sku = int(sku)
+            result = dao.find_document(sku)
+            elemcount = len(result)
+            if result is not None:
+                return json_response(document=result,count=elemcount)
+            else:
+                return json_response(http_status=500, message='Some errors occurred in the process')
+        except Exception:
+            return json_response(http_status=422, message="Invalid parameter")
+
+
+@app.route('/snp/webhooks', methods=['POST'])
+def post_document():
+    if request.method == 'POST':
+        json = request.get_json()
+        dao = Dao()
+        try:
+            if dao.insert_document(json):
+                return json_response(http_status=201, message='Record inserted successfully')
+            else:
+                return json_response(http_status=500, message='Some errors occurred in the process')
+        except Exception:
+            return json_response(http_status=422, message='Some errors occurred in the process')
+
+
+
+
+@app.route('/snp/webhooks/<sku>', methods=['DELETE'])
+def delete_document(sku):
+    if request.method == 'DELETE':
+        dao = Dao()
+        try:
+            if dao.delete_document(sku):
+                return json_response(http_status=202, message='Record deleted successfully.')
+            else:
+                return json_response(http_status=500, message='Some errors occurred in the process.')
+        except Exception:
+            return json_response(http_status=422, message='Invalid parameter to delete.')
+
+
+@app.errorhandler(404)
+def error_404(e):
+    return json_response(http_status=404, message='Unknown URL')
+
+
+if __name__ == '__main__':
+    app.run(port=3000)
